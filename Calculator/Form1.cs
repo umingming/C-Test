4d2f8ab9202b3d61df304a14d1dbe04d4643cdd8
umingming <u_0112@naver.com>
2022-04-01 15:41:29 +0900

    Add calculate()

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -19,12 +19,93 @@ namespace Calculator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello");
+            Button btnTmp = (Button)sender;
+            textBox1.Text += btnTmp.Text;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            string[] plusSplit = textBox1.Text.Split('+');
+
+            double result = 0;
+
+            for(int i=0; i<plusSplit.Length; i++)
+            {
+                string[] minusSplit = plusSplit[i].Split('-');
+                double minusSplitResult = 0;
+
+                for(int j=0; j<minusSplit.Length; j++)
+                {
+                    string[] multiSplit = minusSplit[j].Split('*');
+                    double multiSplitResult = 0;
+
+                    for(int k=0; k<multiSplit.Length; k++)
+                    {
+                        string[] divSplit = multiSplit[k].Split('/'); 
+                        double divSplitResult = 0;
+
+                        for(int l=0; l<divSplit.Length; l++)
+                        {
+                            if (l == 0)
+                            {
+                                divSplitResult += Convert.ToDouble(divSplit[l]);
+                            }
+                            else
+                            {
+                                divSplitResult /= Convert.ToDouble(divSplit[l]);
+                            }
+                        }
+
+                        multiSplit[k] = Convert.ToString(divSplitResult);
+
+                        if(k == 0)
+                        {
+                            multiSplitResult += Convert.ToDouble(multiSplit[k]);
+                        }
+                        else
+                        {
+                            multiSplitResult *= Convert.ToDouble(multiSplit[k]);
+                        }
+                    }
+
+                    minusSplit[j] = Convert.ToString(multiSplitResult);
+
+                    if(j == 0)
+                    {
+                        minusSplitResult += Convert.ToDouble(minusSplit[j]);
+                    }
+                    else
+                    {
+                        minusSplitResult -= Convert.ToDouble(minusSplit[j]);
+                    }
+
+                }
+                plusSplit[i] = Convert.ToString(minusSplitResult);
+
+                Console.WriteLine(plusSplit[i]);
+                result += Convert.ToDouble(plusSplit[i]); 
+            }
+            label2.Text = Convert.ToString(result);
         }
     }
 }
