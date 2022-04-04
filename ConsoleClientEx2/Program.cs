7463b493524df891740ff3282b5667f94dea6aea
umingming <u_0112@naver.com>
2022-04-04 13:29:59 +0900

    Create client program sending message infinitely

---
+++ ConsoleClientEx2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleClientEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("클라이언트");
+            }
+            
+        }
+    }
+}
